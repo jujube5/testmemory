36892a37bfc4e2fdebf0c86051aaea83fc592daf
jujube5 <nataly101084@gmail.com>
2023-04-20 21:15:39 +0400

    Update meminfo_test.go

--- meminfo_test.go
+++ meminfo_test.go
@@ -1,6 +1,7 @@
 package testmemory
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -11,5 +12,27 @@ func TestStringToInt(t *testing.T) {
 		t.Errorf("Result was incorrect, got: %d, want: %d.", str_to_int, 1001)
 	} else {
 		Colorize(CyanBrightFont, "StringToInt -- OK!")
+	}
+}
+
+func TestParseRaw(t *testing.T) {
+	key, value := ParseRaw("MemTotal:        7815804 kB")
+	if key == "MemTotal" || value == 7815804 {
+		Colorize(CyanBrightFont, "ParseRaw -- OK!")
+	} else {
+		Colorize(RedBrightFont, "ParseRaw -- FAIL!")
+		t.Errorf("Result was incorrect.")
+	}
+}
+
+func TestReadMemInfo(t *testing.T) {
+	meminfo := ReadMemInfo()
+	extract_type := reflect.TypeOf(meminfo).Elem()
+    extract_name := extract_type.Name()
+	if extract_name != "MemInfo" {
+		Colorize(RedBrightFont, "ReadMemInfo -- FAIL!")
+		t.Errorf("Result was incorrect, got: %s, want: %s.", extract_name, "MemInfo")
+	} else {
+		Colorize(CyanBrightFont, "ReadMemInfo -- OK!")
 	}
 }
